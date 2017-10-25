f079ebb144176b360942804b19d4cd0863887d26
Harshit Kumar <hrsht.kumar1@gmail.com>
2017-10-25 19:42:39 +0530

    Adding readSmses helper

--- android/src/main/java/hashit/reactnative/sms/Module.java
+++ android/src/main/java/hashit/reactnative/sms/Module.java
@@ -9,20 +9,29 @@ import com.facebook.react.bridge.ReactMethod;
 import java.util.HashMap;
 import java.util.Map;
 
-public class Module extends ReactContextBaseJavaModule {
+import android.content.ContentResolver;
+import android.database.Cursor;
+import android.net.Uri;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+
+public class Module extends ReactContextBaseJavaModule {
+  
   private static final String DURATION_SHORT_KEY = "SHORT";
   private static final String DURATION_LONG_KEY = "LONG";
-
+  
   public Module(ReactApplicationContext reactContext) {
     super(reactContext);
   }
-
+  
   @Override
   public String getName() {
     return "SmsManager";
   }
-
+  
   @Override
   public Map<String, Object> getConstants() {
     final Map<String, Object> constants = new HashMap<>();
@@ -30,9 +39,39 @@ public class Module extends ReactContextBaseJavaModule {
     constants.put(DURATION_LONG_KEY, Toast.LENGTH_LONG);
     return constants;
   }
-
+  
   @ReactMethod
   public void show(String message, int duration) {
     Toast.makeText(getReactApplicationContext(), message, duration).show();
+  }
+  
+  @ReactMethod 
+  public void readSmses(int skip, int limit, Callback fail, Callback success){
+    
+    try{
+      Cursor cursor = getCurrentActivity().getContentResolver().query(Uri.parse("content://sms/"),null, null, null, null);
+      
+      JSONArray messages = new JSONArray();
+      int c = 0;
+      while (cursor.moveToNext()) {
+        c++;
+        // Long dateTime = Long.parseLong(cursor.getString(cursor.getColumnIndex("date")));
+        // String message = cursor.getString(cursor.getColumnIndex("body"));
+        JSONObject json;
+        json = getJsonFromCursor(cursor);
+        messages.put(json);
+        
+        
+        
+      }
+      cursor.close();
+      try {
+        success.invoke(c, messages.toString());
+      } catch(Exception err){
+        fail.invoke(err);
+      }
+    } catch(JSONException e){
+      fail.invoke(e);
+    }
   }
 }
---
+++ android/src/main/java/hashit/reactnative/sms/SmsBroadcastReciever.java
@@ -0,0 +1,11 @@
+package hashit.reactnative.sms;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.telephony.SmsMessage;
+
+class SmsBroadcastReciever {
+    
+}

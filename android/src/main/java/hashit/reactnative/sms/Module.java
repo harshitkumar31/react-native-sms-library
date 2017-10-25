670fdc3470454c2ebf0e3c3d3d76ea1f2aca053d
Harshit Kumar <hrsht.kumar1@gmail.com>
2017-10-25 19:51:25 +0530

    Adding readSmses helper

--- android/src/main/java/hashit/reactnative/sms/Module.java
+++ android/src/main/java/hashit/reactnative/sms/Module.java
@@ -74,4 +74,37 @@ public class Module extends ReactContextBaseJavaModule {
       fail.invoke(e);
     }
   }
+  
+  private JSONObject getJsonFromCursor(Cursor cur) {
+    JSONObject json = new JSONObject();
+    
+    int nCol = cur.getColumnCount();
+    String[] keys = cur.getColumnNames();
+    try
+    {
+      for (int j = 0; j < nCol; j++)
+      switch (cur.getType(j)) {
+        case 0:
+        json.put(keys[j], null);
+        break;
+        case 1:
+        json.put(keys[j], cur.getLong(j));
+        break;
+        case 2:
+        json.put(keys[j], cur.getFloat(j));
+        break;
+        case 3:
+        json.put(keys[j], cur.getString(j));
+        break;
+        case 4:
+        json.put(keys[j], cur.getBlob(j));
+      }
+    }
+    catch (Exception e)
+    {
+      return null;
+    }
+    
+    return json;
+  }
 }
